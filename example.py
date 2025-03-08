c943e5461d453a6a5ac5aba8bc7ab1f89d9e2821
Edouard <edouard.delaire@icloud.com>
2025-03-08 15:15:31 -0500

    Improve support of multiple backend

--- example.py
+++ example.py
@@ -1,18 +1,15 @@
 from pymatbridge import PyMatBridge
 
-
-bridge = PyMatBridge()
+bridge = PyMatBridge(backend=["python", "matlab"])
 bridge.load_python_module('math')
 
-def square(x):
-    return x*x
+print("Sin(3.14) \t= " , bridge.call("math.sin", 3.14) ) 
 
-def sum_of_square(n):
-    return sum( square(x) for x in range(1,n) )
-
-
-print( f"Sin(3.14) = {0:.2f} ".format( bridge.call("math.sin", 3.14)))
+def sum_of_square(n:int):
+    return sum( x*x for x in range(1,n+1) )
 
 bridge.link_python_function('sum_of_square', sum_of_square)
-print("Sum of square: ", bridge.call("sum_of_square", 10 )) 
+print("Sum of square \t= ", bridge.call("sum_of_square", 20 )) 
 
+
+print("randn(3) \t= " , bridge.call("randn", 3) ) 
--- pymatbridge/pymatbridge.py
+++ pymatbridge/pymatbridge.py
@@ -1,22 +1,101 @@
 import builtins
 import importlib
 from types import ModuleType, FunctionType
+from typing import Literal, Optional
+
 import matlab.engine
 
 class PyMatBridge:
 
     loaded_python_module: dict[str, ModuleType] 
     eng: matlab.engine.MatlabEngine | None
+    backend: list[str]
 
-    def __init__(self):
+    
+    def __init__(self, backend: Optional[ Literal["python", "matlab"] | list[Literal["python", "matlab"]]] = None ):
+
+        self.backend = []
+        self.set_backend(backend)
 
         self.eng = None
         self.loaded_python_module = {} 
+
+    def set_backend(self, backends: Optional[ Literal["python", "matlab"] | list[Literal["python", "matlab"]]]):
+
+        if backends is None:
+            self.backend = []
+            return
+        
+        if isinstance(backends, str):
+            backends = [backends]
+        
+        for backend in backends:
+            match backend:
+                case 'python' | 'matlab': 
+                    self.backend.append(backend)
+                case _:
+                    raise NotImplementedError(f"'{backend}' is not a supported backed. ")
+
+
+    def call(self, func_name:str, *args, **kwargs) -> any:
+        """ 
+            Call the function  func_name using the defined backend 
+        """
+
+        if self.backend is None or len(self.backend) == 0 :
+              raise ValueError(f"No backend available.")
+
+        for iBackend, backend_name in enumerate(self.backend, start = 1):
+            
+            # print(f"Backend {iBackend} : calling {backend_name}")
+
+            match backend_name:
+                case "python":
+                    try:
+                        return self.call_python(func_name, *args, **kwargs)
+                    
+                    except (NameError,ValueError) as e:
+                        pass
+                case "matlab": 
+
+                    if not self.is_matlab_started():
+                        self.start_matlab()
+
+                    try:
+                        return self.call_matlab(func_name, *args, **kwargs)
+                    except NameError as e:
+                        pass  
+                case _:          
+                    raise NotImplementedError(f"'{backend_name}' is not a supported backed. ")
+
+        raise ValueError(f"Function '{func_name}' not found.")
+
+    # All functions related to Matlab 
 
     def start_matlab(self):
         print('Starting matlab engine')
         self.eng = matlab.engine.start_matlab()
 
+    def is_matlab_started(self):
+        if self.eng is None:
+            return False
+        
+        return self.eng._check_matlab()
+    def call_matlab(self, func_name:str, *args, **kwargs) -> any:
+        if not self.is_matlab_started():
+            self.start_matlab()
+
+        matlab_args = [matlab.double(arg) if isinstance(arg, (list, tuple)) else arg for arg in args]
+        return self.eng.feval(func_name, *matlab_args)
+
+
+    def stop_matlab(self):
+        if self.is_matlab_started():
+            print('Stopping matlab engine')
+            self.eng.exit()
+
+
+    # All functions related to Python 
     def load_python_module(self, module_name:str, alias:str|None = None) -> bool: 
         alias = module_name if alias is None else alias
 
@@ -33,16 +112,20 @@ class PyMatBridge:
         
         self.loaded_python_module[alias] = module
         return True
-    def link_python_function(self, alias:str, func):
+    
+    def link_python_function(self, alias:str, func:FunctionType) -> bool:
+        if not callable(func):
+            print(f'{alias} is not callable')
+            return False
         if alias in self.loaded_python_module : 
             print(f' Function {alias} is already loaded')
             return True
         
         self.loaded_python_module[alias] = func
+        return True
 
 
-
-    def get_loaded_python_module(self): 
+    def get_loaded_python_module(self)  -> list: 
         return list(self.loaded_python_module.keys())
 
     def get_python_module(self,  module_name:str) -> ModuleType | FunctionType | None:
@@ -58,28 +141,7 @@ class PyMatBridge:
 
         # We check if the parent module is loaded
         module = self.get_python_module(parent_module)
-        return getattr(module, sub_module)
-
-    def call(self, func_name, *args, **kwargs):
-
-        """ Try calling a Python function first, then fall back to MATLAB. """
-        try:
-            return self.call_python(func_name, *args, **kwargs)
-        except NameError as e:
-            print(str(e))
-            pass
-
-        print("Calling matlab function")
-        if self.eng is None:
-            self.start_matlab()
-
-        try:
-
-            matlab_args = [matlab.double(arg) if isinstance(arg, (list, tuple)) else arg for arg in args]
-            return self.eng.feval(func_name, *matlab_args)
-        
-        except matlab.engine.MatlabExecutionError:
-            raise ValueError(f"Function '{func_name}' not found in Python or MATLAB.")
+        return getattr(module, sub_module, None)
 
 
     def call_python(self, func_name:str, *args, **kwargs) -> any:
@@ -96,6 +158,3 @@ class PyMatBridge:
         
         raise NameError(f"Function '{func_name}' not found")
             
-
-    def stop_matlab(self):
-        self.eng.exit()
